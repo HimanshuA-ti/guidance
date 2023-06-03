54cf0a1c12183b230557c1746a32739ac622b0db
HimanshuA-ti <himanshu.aggarwal@trilogy.com>
2023-06-03 20:05:48 +0000

    fix somethings

--- guidance/llms/caches/_diskcache.py
+++ guidance/llms/caches/_diskcache.py
@@ -5,13 +5,14 @@ import platformdirs
 
 from guidance.llms.caches import Cache
 
-def user_cache_dir(appname):
-    return "/tmp/cache"
 
 class DiskCache(Cache):
     """DiskCache is a cache that uses diskcache lib."""
     def __init__(self, llm_name: str):
         print("Inside my guidance")
+        print(llm_name)
+        cache_dir = "/tmp/cache/"+ f"{llm_name}.diskcache"
+        print(cache_dir)
         self._diskcache = diskcache.Cache(
             os.path.join(
                 user_cache_dir("guidance"), f"{llm_name}.diskcache"
